5206f6d7ed3552578a3d2d8afd866e07d8ca99c5
shivankt66 <98093628+shivankt66@users.noreply.github.com>
2025-03-25 12:46:41 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,12 +3,18 @@ import { useRef } from "react";
 export const Search = ({fn})=>{
 
     const artist = useRef();
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+          fn(artist.current.value);
+        }
+      };
     
     return (
     <div className="row">
 
         <div className="col-8">
-        <input ref={artist} type ='text' className="form-control" placeholder="Search Song OR Artist Name"/>
+        <input ref={artist} type ='text' className="form-control" placeholder="Search Song OR Artist Name" onKeyDown={handleKeyPress}/>
         </div>
 
         <div className="col-2">
@@ -16,7 +22,6 @@ export const Search = ({fn})=>{
         fn(artist.current.value);
     }}>Search</button>
         </div>
-
     </div>
     );
     
