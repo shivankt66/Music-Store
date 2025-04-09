0c67a141814c06b9dd6a5fd99cb808ab8f0a9284
shivankt66 <98093628+shivankt66@users.noreply.github.com>
2025-04-09 21:04:52 +0530

    Update api-client.js

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -3,6 +3,5 @@ import axios from 'axios';
 export async function getSongs(termName){
     const URL = `https://itunes.apple.com/search?term=${termName}&limit=25&country=in`;
     const reponse = await axios.get(URL);
-    console.log(reponse.data['results']);
     return reponse.data['results'];
 }
